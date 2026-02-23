ad978bc6af2cdee7bc477865397a239b4c767c90
jopeif <joao.pedro.lima06@aluno.ifce.edu.br>
2026-02-22 21:35:38 -0300

    refactor: simplify CORS configuration to allow all origins

--- src/server.ts
+++ src/server.ts
@@ -17,10 +17,7 @@ const app = express()
 
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:5500',
-        'http://127.0.0.1:5500'
+        "*"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
